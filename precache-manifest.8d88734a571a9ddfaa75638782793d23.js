949679d0f43b54e5d2cc6990997ff0a5980f0370
Miroslav P <miroslav.petkevic@gmail.com>
2019-01-26 14:25:33 +0200

    Updates

---
+++ precache-manifest.8d88734a571a9ddfaa75638782793d23.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "f51252718a4dada7f24907ed0a3a97be",
+    "url": "/burger-app/static/media/special.f5125271.png"
+  },
+  {
+    "revision": "ad7538d118c2c5ab5bbbd4e0fa9a52b2",
+    "url": "/burger-app/static/media/main_dish.ad7538d1.png"
+  },
+  {
+    "revision": "d5055ab8985a1dee48a48f8d0f96bf93",
+    "url": "/burger-app/static/media/drinks.d5055ab8.png"
+  },
+  {
+    "revision": "12d7c29cef70a1e19b876f20e03b7b16",
+    "url": "/burger-app/static/media/cake.12d7c29c.png"
+  },
+  {
+    "revision": "970a8769db42c56151f2",
+    "url": "/burger-app/static/js/runtime~main.970a8769.js"
+  },
+  {
+    "revision": "6e95353805455b64a636",
+    "url": "/burger-app/static/js/main.6e953538.chunk.js"
+  },
+  {
+    "revision": "b32c1dbbcc2c2e68e03b",
+    "url": "/burger-app/static/js/1.b32c1dbb.chunk.js"
+  },
+  {
+    "revision": "6e95353805455b64a636",
+    "url": "/burger-app/static/css/main.9a62e4e7.chunk.css"
+  },
+  {
+    "revision": "c0634d0530b1a8ff1df0bebaba1666a2",
+    "url": "/burger-app/index.html"
+  }
+];

e9c81c890bbe14cfbba7a91a01805c7c40d135d0
Miroslav P <miroslav.petkevic@gmail.com>
2019-01-26 14:31:37 +0200

    Modified Api URL

--- src/App.js
+++ src/App.js
@@ -6,95 +6,105 @@ import Statistics from './components/Statistics';
 import Menu from './components/Menu';
 import axios from 'axios';
 
-const MENU_URL = 'http://enigmatic-cliffs-25405.herokuapp.com/menu';
+const MENU_URL = 'https://enigmatic-cliffs-25405.herokuapp.com/menu';
 
 class App extends Component {
   state = {
     navigation: ['Orders', 'Statistics', 'Settings'],
     activeTab: 0,
     categories: ['drinks', 'dishes', 'deserts', 'special'],
-    tables: ['Two person table', 'Family table', 'Progers Table', 'Coffee Table'],
+    tables: [
+      'Two person table',
+      'Family table',
+      'Progers Table',
+      'Coffee Table'
+    ],
     activeTable: 0,
     activeCategory: 0,
     orders: [],
     menu: null,
     total: 0
-  }
+  };
 
-  componentDidMount = async() => {
-    const {data} = await axios.get(MENU_URL); // palaukim, kol ateis doumenys
+  componentDidMount = async () => {
+    const { data } = await axios.get(MENU_URL); // palaukim, kol ateis doumenys
     // doumenys pasieksime kaip res.data
-    console.log(data)
-    this.setState({menu: data.menu});
-  }
+    console.log(data);
+    this.setState({ menu: data.menu });
+  };
 
-  addOrder = (menuItem)=>{
-    let n=false;
-    const orders = this.state.orders.map((order)=>{
-      if(order.name===menuItem.name && order.table===this.state.activeTable){
-        n=true;
-        return {...order,count:order.count+1}
-      }else{
-        return order
+  addOrder = menuItem => {
+    let n = false;
+    const orders = this.state.orders.map(order => {
+      if (
+        order.name === menuItem.name &&
+        order.table === this.state.activeTable
+      ) {
+        n = true;
+        return { ...order, count: order.count + 1 };
+      } else {
+        return order;
       }
     });
-    if(n){
-      this.setState({orders})
-    }else{
-      const orders =[...this.state.orders,{...menuItem, table:this.state.activeTable, count:1}];
-      this.setState({orders})
+    if (n) {
+      this.setState({ orders });
+    } else {
+      const orders = [
+        ...this.state.orders,
+        { ...menuItem, table: this.state.activeTable, count: 1 }
+      ];
+      this.setState({ orders });
     }
   };
 
-  addSpecial = (e,item) => {
-    e.preventDefault()
+  addSpecial = (e, item) => {
+    e.preventDefault();
     console.log(item);
   };
   checkout = (table, total) => {
     console.log(table);
     const completed = [...this.state.completed];
     this.setState({
-      orders: this.state.orders.filter((order) => {
+      orders: this.state.orders.filter(order => {
         if (order.table !== table) {
-          return order
+          return order;
         } else {
-          completed.push(order)
+          completed.push(order);
         }
       })
-    })
-    this.setState({total: this.state.total + total, completed})
+    });
+    this.setState({ total: this.state.total + total, completed });
   };
 
-  removeOrder = (order) => {
-    if(order.count > 1) {
-      const orders = this.state.orders.map((item)=>{
-        if(item.name===order.name && item.table===this.state.activeTable){
-          return {...item,count:item.count-1}
-        }else{
+  removeOrder = order => {
+    if (order.count > 1) {
+      const orders = this.state.orders.map(item => {
+        if (item.name === order.name && item.table === this.state.activeTable) {
+          return { ...item, count: item.count - 1 };
+        } else {
           return item;
         }
       });
-      this.setState({orders});
+      this.setState({ orders });
     } else {
-      const orders = this.state.orders.filter((item)=>{
-        if(item.name !==order.name && item.table ===this.state.activeTable) {
+      const orders = this.state.orders.filter(item => {
+        if (item.name !== order.name && item.table === this.state.activeTable) {
           return item;
         }
       });
-      this.setState({orders});
+      this.setState({ orders });
     }
-  }
+  };
 
-  activeTabChange = activeTab => this.setState({activeTab})
+  activeTabChange = activeTab => this.setState({ activeTab });
 
-  activeTableChange = activeTable => this.setState({activeTable});
+  activeTableChange = activeTable => this.setState({ activeTable });
 
-  onCateroryChange = activeCategory => this.setState({activeCategory});
+  onCateroryChange = activeCategory => this.setState({ activeCategory });
 
   checkout = (tableTotal, table) => {
-    this.setState({total: this.state.total + tableTotal});
-
-  }
+    this.setState({ total: this.state.total + tableTotal });
+  };
 
   render() {
     const content = [
@@ -107,16 +117,11 @@ class App extends Component {
         removeOrder={this.removeOrder}
         checkout={this.checkout}
       />,
-      <Statistics
-        key="2"
-      />,
-      <Settings
-        key="3"
-        addSpecial={this.addSpecial}
-      />
-      ];
+      <Statistics key="2" />,
+      <Settings key="3" addSpecial={this.addSpecial} />
+    ];
 
-    const {menu, categories, activeCategory} = this.state;
+    const { menu, categories, activeCategory } = this.state;
     return (
       <div className="App">
         <Header
